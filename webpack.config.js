7822c99b93a1a1df4401e03260912fb02b4eb3dc
Vitor Reis <vitor.reis@izap.com.br>
2022-12-07 17:08:46 -0300

    feat: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path'); 
+
+module.exports = { 
+  mode: 'development',
+  entry: "./src/index.js", 
+  output: { 
+    "path": path.resolve(__dirname, 'dist'), 
+    "filename": "bundle.js" 
+  }, 
+  module: { 
+    rules: [ 
+      { 
+        test: /\.css$/, 
+        use: [ "style-loader", "css-loader" ] 
+      }, 
+      { 
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        use: { 
+          "loader": "babel-loader", 
+        } 
+      }, 
+    ]
+
+  }
+};
