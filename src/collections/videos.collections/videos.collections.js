da92ddaec85e2cd16b77506a1c06d8fa4789b053
Vitor Reis <vitor.reis@izap.com.br>
2022-12-08 06:16:46 -0300

    refactor: adapt datials model to fetch and parse data

--- src/collections/videos.collections/videos.collections.js
+++ src/collections/videos.collections/videos.collections.js
@@ -1,10 +1,12 @@
 import Backbone from 'backbone';
+import $ from 'jquery'
 import { VideoModel } from '../../models/index.js'
 
 const VideosCollection = Backbone.Collection.extend({
   initialize: function(_, options){
-    this.screen_name = options.screen_name;
     Backbone.Collection.prototype.initialize.call(this, arguments);
+    this.screen_name = options.screen_name;
+    this.search = options.search
   },
 
   model: VideoModel,
@@ -12,36 +14,42 @@ const VideosCollection = Backbone.Collection.extend({
   urlRoot: process.env.YOUTUBE_URL,
 
   url: function(){
-    const apiKey = process.env.API_KEY
-    return this.urlRoot + `?part=snippet&key=${apiKey}&q=IronMaiden`
+    return this.urlRoot + '/search'
+  },
+  
+  getParams: function(){
+    const apiKey = process.env.YOUTUBE_API_KEY
+    return ({
+      part: 'snippet', 
+      q: this.search,
+      key: apiKey,
+    })
+  },
+
+  parse: function(response){
+    return  response.items.map(video => ({
+      videoId: video.id.videoId,
+      channelId: video.snippet.channelId,
+      channelTitle: video.snippet.channelTitle,
+      description: video.snippet.description,
+      publishTime: video.snippet.publishTime,
+      publishedAt: video.snippet.publishedAt,
+      thumbnails: video.snippet.thumbnails.default.url,
+      title: video.snippet.title,
+    }))
   },
 
   fetch: function(options){
     options = options || {};
     options = {
       ...options,
-      success: (_, response) => {
-        if(response.error){
-          console.log("error", response.error.message)
-          return
-        }
-
-        return  response.items.map(video => ({
-          videoId: video.id.videoId,
-          channelId: video.snippet.channelId,
-          channelTitle: video.snippet.channelTitle,
-          description: video.snippet.description,
-          publishTime: video.snippet.publishTime,
-          publishedAt: video.snippet.publishedAt,
-          thumbnails: video.snippet.thumbnails.default.url,
-          title: video.snippet.title,
-        }))
-      },
-      complete: (_, response) => {
-        console.log("completeeee: ", response)
-      }
+      type: "GET",
+      dataType: "JSON",
+      jsonp: false,
+      jsonpCallback: false,
+      contentType:'jsonp',
+      data: $.param(this.getParams()),
     }
-    options.dataType = 'jsonp';
     Backbone.Collection.prototype.fetch.call(this, options);
   },
 });
