20ae73a5c5f486c311245786e36eea41ddaa28a0
Vitor Reis <vitor.reis@izap.com.br>
2022-12-07 22:24:42 -0300

    refactor: video view

--- src/views/video.view.js
+++ src/views/video.view.js
@@ -1,17 +1,17 @@
+import $ from 'jquery'
+import _ from 'underscore'
+
 const VideoView = Backbone.View.extend({
   tagName: "li",
   className: 'video',
-  //template: _.template($('#video-template').html()),
-  initialize: function() {
-      this.render();
-  },
+  template: _.template($('#video-template').html()),
+  
   render: function() {
-    if(this?.model){
-      return this?.model?.toJSON();
-    }
-
-    return ''
-  }
+    var innerHTML = this.template(this?.model.toJSON())
+    this.$el.html(innerHTML)
+    
+    return this
+  },
 });
 
 export {
