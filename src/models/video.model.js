0df0ef1b56298168203e67fc6c3317ea4004f3f2
Vitor Reis <vitor.reis@izap.com.br>
2022-12-08 00:09:49 -0300

    refactor: video model

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,23 +1,22 @@
-const VideoModel = Backbone.Model.extend({
-  defaults: function() {
-    return {
-      videoId: '',
-      title: "",
-      description: "",
-      thumbnail: "",
-      thumbnail: "",
-      channelTitle: "",
-      publishedAt: "",
-      channelId: "",
-    };
-  },
+import Backbone from "backbone";
 
+const VideoModel = Backbone.Model.extend({
   getUrlVideo: function(){
-    return `https://youtube.com/watch?v=${this.videoId}`
+    if(this.get("videoId")){
+      return `https://youtube.com/watch?v=${this.get("videoId")}`
+    }
+
+    return '#'
   },
   
   toJSON: function(){
-    return this
+    return {
+      title: this.get('title'),
+      url: this.getUrlVideo(),
+      channelTitle: this.get('channelTitle'),
+      description: this.get('description'),
+      videoId: this.get('videoId'),
+    }
   }
 })
 
